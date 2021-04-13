a26ab06df56325defc1df97974816405202f8d6f
Adrian Perez <me@adrianperez.me>
2021-04-13 07:39:08 +0200

    Bump version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_rocketchat';
-$plugin->version = 2020120900;
+$plugin->version = 2020120901;
 $plugin->requires = 2020061500;
-$plugin->release = 'v3.9-r1';
+$plugin->release = 'v3.9-r2';
 $plugin->maturity = MATURITY_STABLE;
